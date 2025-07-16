11c5a5823e8ace93432498b72ba4cfb7c5ab5e3d
Amul Sul <sulamul@gmail.com>
2025-07-16 18:37:59 +0530

    WIP -  added astream_waldump.c file

--- src/bin/pg_waldump/pg_waldump.h
+++ src/bin/pg_waldump/pg_waldump.h
@@ -22,6 +22,22 @@ typedef struct XLogDumpPrivate
 	XLogRecPtr	startptr;
 	XLogRecPtr	endptr;
 	bool		endptr_reached;
+
+	/* Fields required to read WAL from archive */
+	char	   *archive_name;	/* Tar archive name */
+	int			archive_fd;		/* File descriptor for the open tar file */
+
+	astreamer  *archive_streamer;
+	StringInfo	archive_streamer_buf;	/* Buffer for receiving WAL data */
+	XLogRecPtr	archive_streamer_read_ptr; /* Populate the buffer with records
+											  until this record pointer */
 } XLogDumpPrivate;
 
-#endif		/* end of PG_WALDUMP_H */
+#endif							/* end of PG_WALDUMP_H */
+
+extern astreamer *astreamer_waldump_content_new(astreamer *next,
+												XLogRecPtr startptr,
+												XLogRecPtr endptr,
+												XLogDumpPrivate *private);
+extern int	astreamer_wal_read(char *readBuff, XLogRecPtr startptr, Size count,
+							   XLogDumpPrivate *private);
