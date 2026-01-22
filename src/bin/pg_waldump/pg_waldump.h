a44b0547a720a89ae6735d05d73d1fdf22da38c6
Amul Sul <sulamul@gmail.com>
2026-01-22 10:28:32 +0530

    Refactor: pg_waldump: Move some declarations to new pg_waldump.h
    This change prepares for a second source file in this directory to
    support reading WAL from tar files. Common structures, declarations,
    and functions are being exported through this include file so
    they can be used in both files.

---
+++ src/bin/pg_waldump/pg_waldump.h
@@ -0,0 +1,25 @@
+/*-------------------------------------------------------------------------
+ *
+ * pg_waldump.h - decode and display WAL
+ *
+ * Copyright (c) 2026, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *		  src/bin/pg_waldump/pg_waldump.h
+ *-------------------------------------------------------------------------
+ */
+#ifndef PG_WALDUMP_H
+#define PG_WALDUMP_H
+
+#include "access/xlogdefs.h"
+
+/* Contains the necessary information to drive WAL decoding */
+typedef struct XLogDumpPrivate
+{
+	TimeLineID	timeline;
+	XLogRecPtr	startptr;
+	XLogRecPtr	endptr;
+	bool		endptr_reached;
+} XLogDumpPrivate;
+
+#endif							/* end of PG_WALDUMP_H */
